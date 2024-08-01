58c4bc0cb0b78664e37cc61860a57a87b2db84b7
saymamutovsaidakmol@gmail.com <116891087+saidkamol2006@users.noreply.github.com>
2024-08-01 07:50:49 +0500

    Sizga m x naccountsmijozning bankdagi pul miqdori qancha bo'lgan butun sonli panjara beriladiaccounts[i][j]i​​​​​​​​​​​th​​​​j​​​​​​​​​​​th. mijozning boyligini qaytaring.
    Mijozning boyligi - bu ularning barcha bank hisoblaridagi pul miqdori. Eng boy mijoz-bu maksimal boylikka ega bo'lgan mijoz.

--- problem1.py
+++ problem1.py
@@ -1,2 +1,26 @@
-ls = [1,2,3,4]
-print(ls)
+bank = [[1, 2, 3], [3, 2, 1, 3], [4, 5, 6]]
+
+mijoz1, mijoz2, mijoz3 = [mijoz for mijoz in bank]
+
+print("Mijoz1:", mijoz1)
+print("Mijoz2:", mijoz2)
+print("Mijoz3:", mijoz3)
+
+summa1 = 0
+for i in mijoz1:
+    summa1 += i
+
+summa2 = 0
+for i in mijoz2:
+    summa2 += i
+    
+summa3 = 0
+for i in mijoz3:
+    summa3 += i
+
+if summa1 > summa2  and summa1 > summa3:
+    print(f"Birinchi mijozni summasi eng katta -> {summa1}")
+elif summa2 > summa1 and summa2 > summa3:
+    print(f"Ikkinchi mijozni summasi eng katta -> {summa2}")
+elif summa3 > summa1 and summa3 > summa2:
+    print(f"Uchinchi mijozni summasi eng katta -> {summa3}")
